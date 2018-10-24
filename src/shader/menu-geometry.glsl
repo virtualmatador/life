facfafc0a92fbc57b98eac6904fa3a36ed4e8731
Ali Asadpoor <virtualmatador@yahoo.com>
2018-10-24 01:11:48 -0500

    Show speed

--- src/shader/menu-geometry.glsl
+++ src/shader/menu-geometry.glsl
@@ -1,10 +1,10 @@
 #version 450
 
 layout(points) in;
-layout(line_strip, max_vertices = 2) out;
+layout(line_strip, max_vertices = 16) out;
 layout(std430, binding = 0) buffer SSB0
 {
-	float font[];
+	float font[][8][2];
 };
 
 in int g_character[];
@@ -15,17 +15,16 @@ out vec4 f_color;
 void main()
 {
 	f_color = g_color[0];
-	int fnt = g_character[0] * 8 * 2;
 	bool bInit = false;
-	for (int i = 0; i < 8 && (font[fnt + i * 2] > -0.5 || font[fnt + i * 2 + 1] > -0.5); ++i)
+	for (int i = 0; i < 8 && (font[g_character[0]][i][0] > -0.5 || font[g_character[0]][i][1] > -0.5); ++i)
 	{
-		if (font[fnt + i * 2] > -0.5 && font[fnt + i * 2 + 1] > -0.5)
+		if (font[g_character[0]][i][0] > -0.5 && font[g_character[0]][i][1] > -0.5)
 		{
 			if (bInit)
 				EmitVertex();
 			gl_Position = vec4(
-				gl_in[0].gl_Position.x + font[fnt + i * 2] * g_scale[0], 
-				gl_in[0].gl_Position.y + font[fnt + i * 2 + 1] * g_scale[0],
+				gl_in[0].gl_Position.x + font[g_character[0]][i][0] * g_scale[0], 
+				gl_in[0].gl_Position.y + font[g_character[0]][i][1] * g_scale[0],
 				0.0, 1.0);
 			EmitVertex();
 			if (bInit)
